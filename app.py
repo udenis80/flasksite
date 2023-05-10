ab57aeadcabc0809655204342d9ff176e7e425ab
udenis80 <udenis80@mail.ru>
2023-05-11 00:23:49 +0300

    new project

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')\
+
+@app.route('/')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
